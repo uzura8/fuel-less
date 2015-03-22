3ea7b3f52961213fb0e9fb6eb43e32c3797cd23b
shingo <uzuranoie@gmail.com>
2015-03-22 20:00:19 +0900

    Add asset configs.

---
+++ config/asset.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'folders' => array(
+		'css' => array('assets/ccss'),
+	),
+	'less_source_dir' => APPPATH.'assets/less/',
+	'less_output_dir' => DOCROOT.'assets/ccss/',
+	'less_compiler' => 'Less\\Compiler_Lessphp',
+);
